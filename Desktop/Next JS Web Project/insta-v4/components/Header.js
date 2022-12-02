27703c41d162146c8c5d04eedd8191dea40588ba
Tianhao Zhang <zhangth1990@hotmail.com>
2022-12-01 22:17:44 -0700

    implement sticky header

--- Desktop/Next JS Web Project/insta-v4/components/Header.js
+++ Desktop/Next JS Web Project/insta-v4/components/Header.js
@@ -8,40 +8,42 @@ import { HomeIcon } from "@heroicons/react/24/solid";
 
 export default function Header() {
     return (
-        <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
-            <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-                <Image
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png"
-                    layout="fill"
-                    className="object-contain"
-                />
-            </div>
-            <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-                <Image
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
-                    layout="fill"
-                    className="object-contain"
-                />
-            </div>
-            <div className="relative mt-1">
-                <div className="absolute top-2 left-2">
-                    <MagnifyingGlassIcon className="h-5 text-gray-500" />
+        <div className="shadow-sm border-b sticky top-0 bg-white z-30">
+            <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+                <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+                    <Image
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png"
+                        layout="fill"
+                        className="object-contain"
+                    />
                 </div>
-                <input
-                    type="text"
-                    placeholder="Search"
-                    className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
-                />
-            </div>
+                <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+                    <Image
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
+                        layout="fill"
+                        className="object-contain"
+                    />
+                </div>
+                <div className="relative mt-1">
+                    <div className="absolute top-2 left-2">
+                        <MagnifyingGlassIcon className="h-5 text-gray-500" />
+                    </div>
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+                    />
+                </div>
 
-            <div className="flex space-x-4 items-center">
-                <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-                <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-                <img
-                    src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                    alt="user_image"
-                    className="h-10 rounded-full cursor-pointer"
-                />
+                <div className="flex space-x-4 items-center">
+                    <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+                    <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+                    <img
+                        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                        alt="user_image"
+                        className="h-10 rounded-full cursor-pointer"
+                    />
+                </div>
             </div>
         </div>
     );
