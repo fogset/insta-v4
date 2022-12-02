408c343277d912d5f55c693b3af627a92dae6887
Tianhao Zhang <zhangth1990@hotmail.com>
2022-12-01 22:02:25 -0700

    add the menu and profile image at the right side of the header

--- Desktop/Next JS Web Project/insta-v4/components/Header.js
+++ Desktop/Next JS Web Project/insta-v4/components/Header.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import React from "react";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import {
+    MagnifyingGlassIcon,
+    PlusCircleIcon,
+} from "@heroicons/react/24/outline";
+import { HomeIcon } from "@heroicons/react/24/solid";
 
 export default function Header() {
     return (
-        <div className="flex items-center justify-between max-w-6xl">
+        <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
             <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png"
@@ -30,7 +34,15 @@ export default function Header() {
                 />
             </div>
 
-            <h1>Right sides</h1>
+            <div className="flex space-x-4 items-center">
+                <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+                <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+                <img
+                    src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                    alt="user_image"
+                    className="h-10 rounded-full cursor-pointer"
+                />
+            </div>
         </div>
     );
 }
