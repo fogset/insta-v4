d2cb73cf50b4c24b9df739843ea47646f29c72ce
Tianhao Zhang <zhangth1990@hotmail.com>
2022-12-01 21:47:45 -0700

    add search bar to the header component

--- Desktop/Next JS Web Project/insta-v4/components/Header.js
+++ Desktop/Next JS Web Project/insta-v4/components/Header.js
@@ -1,26 +1,37 @@
 import Image from "next/image";
 import React from "react";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 export default function Header() {
     return (
-        <div>
-            <div className="flex items-center justify-between max-w-6xl">
-                <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-                    <Image
-                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png"
-                        layout="fill"
-                        className="object-contain"
-                    />
-                </div>
-                <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-                    <Image
-                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
-                        layout="fill"
-                        className="object-contain"
-                    />
-                </div>
-                <h1>Right sides</h1>
+        <div className="flex items-center justify-between max-w-6xl">
+            <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+                <Image
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png"
+                    layout="fill"
+                    className="object-contain"
+                />
             </div>
+            <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+                <Image
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
+                    layout="fill"
+                    className="object-contain"
+                />
+            </div>
+            <div className="relative mt-1">
+                <div className="absolute top-2 left-2">
+                    <MagnifyingGlassIcon className="h-5 text-gray-500" />
+                </div>
+                <input
+                    type="text"
+                    placeholder="Search"
+                    className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+                />
+            </div>
+
+            <h1>Right sides</h1>
         </div>
     );
 }
+//
