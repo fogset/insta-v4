4702768116d8af78ff869923a8700a9f080f6ba9
Tianhao Zhang <zhangth1990@hotmail.com>
2022-12-02 21:13:18 -0700

    create the header and image sections

--- Desktop/Next JS Web Project/insta-v4/components/Post.js
+++ Desktop/Next JS Web Project/insta-v4/components/Post.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { username } from "minifaker";
+import { EllipsisHorizontalIcon } from "@heroicons/react/24/solid";
 
 export default function Post({ img, userImg, caption, username, id }) {
     return (
-        <div>
-            <h1>{username}</h1>
+        <div className="bg-white my-7 border rounded-md">
+            <div className="flex items-center p-5">
+                <img
+                    className="h-12 rounded-full object-cover border p-1 mr-3"
+                    src={userImg}
+                    alt={username}
+                />
+                <p className="font-bold flex-1">{username}</p>
+                <EllipsisHorizontalIcon className="h-5" />
+            </div>
+            <img className="object-cover w-full" src={img} alt="" />
         </div>
     );
 }
