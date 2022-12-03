6845b746605072759f5e7739e9766fdc8cbbd078
Tianhao Zhang <zhangth1990@hotmail.com>
2022-12-03 11:53:24 -0700

    create the mini profile component

--- Desktop/Next JS Web Project/insta-v4/components/Post.js
+++ Desktop/Next JS Web Project/insta-v4/components/Post.js
@@ -30,21 +30,22 @@ export default function Post({ img, userImg, caption, username, id }) {
                 <BookmarkIcon className="btn" />
             </div>
 
-            {/*Post comments */}
+            {/* Post comments */}
+
             <p className="p-5 truncate">
                 <span className="font-bold mr-2">{username}</span>
                 {caption}
-                {/*Post input box */}
-                <form className="flex items-center p-4">
-                    <FaceSmileIcon className="h-7" />
-                    <input
-                        className="border-none flex-1 focus:ring-0"
-                        type="text"
-                        placeholder="Enter your comment..."
-                    />
-                    <button className="text-blue-400 font-bold">Post</button>
-                </form>
             </p>
+            {/*Post input box */}
+            <form className="flex items-center p-4">
+                <FaceSmileIcon className="h-7" />
+                <input
+                    className="border-none flex-1 focus:ring-0"
+                    type="text"
+                    placeholder="Enter your comment..."
+                />
+                <button className="text-blue-400 font-bold">Post</button>
+            </form>
         </div>
     );
 }
